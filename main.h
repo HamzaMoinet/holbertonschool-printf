85316921b93bc7c7d4dccbd78aa41d5184a2bf80
HamzaMoinet <moinethamza0@gmail.com>
2024-11-25 11:26:56 +0100

    main.h

---
+++ main.h
@@ -0,0 +1,17 @@
+#ifndef printf
+#define printf
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+
+typedef struct op
+{
+	char *op;
+	int (*f)(int a, int b);
+} op_t;
+int _printf(const char *format, ...);
+
+#endif
