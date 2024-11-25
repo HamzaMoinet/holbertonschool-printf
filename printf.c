f38430f9061025799d8670a3cda71710af9bc058
BvrGuillaume <bouvier.guillaume53@gmail.com>
2024-11-25 13:36:35 +0100

    printf.c

---
+++ printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _print - function that produces output according to a format
+ * @c:
+ * @s:
+ * @%:
+ */
+
+int _printf(const char *format, ...)
+{
+	
+}
