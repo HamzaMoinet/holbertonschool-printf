7637bc77dea8b735f53c98bb765d3a657df9e956
BvrGuillaume <bouvier.guillaume53@gmail.com>
2024-11-26 21:53:10 +0100

    update

--- printf.c
+++ printf.c
@@ -18,7 +18,7 @@ int _printf(const char *format, ...);
 
 	va_start(args, format);
 
-	for (i = 0; format[i] !='\0'; i++)
+	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] == '%')
 		{
@@ -56,5 +56,7 @@ int _printf(const char *format, ...);
 					printedCount = count + printedCount
 		}
 	}
-	return (printedCount)
+	va_end(args);
+	
+	return (printedCount);
 }
