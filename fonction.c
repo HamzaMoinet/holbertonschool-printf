000b10b190fd84e34c77b78583afc0de26473866
HamzaMoinet <moinethamza0@gmail.com>
2024-11-26 12:09:46 +0100

    test_fontion

--- fonction.c
+++ fonction.c
@@ -2,14 +2,24 @@
 #include <stdio.h>
 #include <stdarg.h>
 /**
- * print_c - prints character
+ * print_char - prints character
  * @args: character argument
- * Return: number of characters
+ *
  */
-int print_c(va_list args)
+void printf_char(va_list ap)
 {
-	int c;
+	char c = va_arg(ap, int);
+		_putchar(c);
+}
 
-	c = va_arg(args, int);
-	return (_putchar(c));
+/**
+ * printf_percent: print modulos
+ *
+ * 
+ */
+void printf_percent(va_list ap)
+{
+	int i = va_arg(ap, int);
+	if (i == 37)
+		_putchar(37);
 }
