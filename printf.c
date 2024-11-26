a38bdb6e69fa3897e78533d23a6e2deadce76f95
BvrGuillaume <bouvier.guillaume53@gmail.com>
2024-11-26 21:48:53 +0100

    update

--- printf.c
+++ printf.c
@@ -2,20 +2,59 @@
 #include <stdarg.h>
 #include "main.h"
 /**
- *
- *
- */
+*
+*
+*/
 
 int _printf(const char *format, ...);
 {
-	va_list args;
-	int count = 0;
+	va list args;
+	int printedCount = 0;
+
 	int i;
 
 	if (format == NULL)
-		return (-1);
+		return (0);
 
 	va_start(args, format);
 
-	for (i = 0; format[i] != '\0'; i++)
+	for (i = 0; format[i] !='\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+				i++;
+			if (format[i] == 'c')
+			{
+				char c = va_arg(args, int);
+
+					int count = _putchar(c)
+
+					printedCount = count + printedCount
+
+			}
+			else if (format[i] == 's')
+			{
+				char *string = va_arg(args, char *);
+
+					int count = _putstring(string)
+
+					printedCount = count + printedCount
+
+			}
+			else
+			{
+				int count = _putchar(format[i])
+
+					printedCount = count + printedCount
+			}
+
+		}
+		else
+		{
+			int count = _putchar(format[i])
+
+					printedCount = count + printedCount
+		}
+	}
+	return (printedCount)
 }
