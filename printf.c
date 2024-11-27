d8c364f0c8d178480c0a3f1d2168ba1db38798d9
HamzaMoinet <moinethamza0@gmail.com>
2024-11-27 09:10:19 +0100

    printf

--- printf.c
+++ printf.c
@@ -1,1 +1,14 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
+#include <stddef.h>
+/**
+ * _printf - recreates the printf function
+ * @format: string with format specifier
+ * Return: number of characters printed
+ */
+int _printf(const char *format, ...) /* A remplir plus tard. */
+{
+
+}
